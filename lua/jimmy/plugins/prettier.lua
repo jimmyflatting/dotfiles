0c367185746fcec7a1c817c83849e09b258cd596
Jimmy Flatting <jimmy.flatting@icloud.com>
2024-11-05 18:12:14 +0100

    new

--- lua/jimmy/plugins/prettier.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
-augroup PrettierAutoCmd
-  autocmd!
-  autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Prettier
-augroup END
-]])
-
---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+-- CONFIGS
+require("config.options")
+require("config.lazy")
+require("config.mappings")
+
+-- PLUGINS
+-- require("core.plugins.nvimtree")
